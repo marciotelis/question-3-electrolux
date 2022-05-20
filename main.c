4e5f6f4fff19b59d30628507501850322a7a37ab
Marcio Telis <marciotelis@garten.com.br>
2022-05-19 23:47:21 -0300

    style: add comments

--- main.c
+++ main.c
@@ -27,19 +27,25 @@ int main(void)
 
 void array_statistics(int input_array[], int input_array_size, int *average, int *maximum, int *minimum, int array_return[], int *array_copy_size_return)
 {
+    /*  Reset all pointers  */
     int array_sum = 0;
     *average = 0;
     *maximum = 0;
     *minimum = 0;
+
+    /*  Set the value of array copy size    */
     *array_copy_size_return = input_array_size;
 
+    /*  reallocates array memory to the same size as the array to be copied */
     array_return = realloc(array_return, (input_array_size * sizeof(int)));
 
+    /*  Copy input array values to array copy   */
     for (int j = 0; j < input_array_size; j++)
     {
         array_return[j] = input_array[j];
     }
 
+    /*  Check which are the largest and smallest elements   */
     for (int i = 0; i < input_array_size; i++)
     {
         array_sum += input_array[i];
@@ -64,5 +70,6 @@ void array_statistics(int input_array[], int input_array_size, int *average, int
         }
     }
 
+    /*  Calculates the average of array values  */
     *average = array_sum / input_array_size;
 }
